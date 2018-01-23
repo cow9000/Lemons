3be57e4482cb96021dbc6f16db2f7c550c05bcf1
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-01-23 12:18:45 -0700

    Initial commit

---
+++ Lemons/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Lemons
+//
+//  Created by Vawdrey, Derek on 1/23/18.
+//  Copyright © 2018 Vawdrey, Derek. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    //insert code here...
+    std::cout << "Hello, World!" << std::endl;
+    
+    
+    return 0;
+}
